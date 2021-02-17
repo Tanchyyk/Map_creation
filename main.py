963d56d9285972ecb4773bfd229de79aed7f9edf
Tania Prysiazhnyk <73783964+Tanchyyk@users.noreply.github.com>
2021-02-17 15:48:58 +0200

    Change module's code

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from creation_of_map import put_points_on_map
 
 
 if __name__ == '__main__':
-    print(put_points_on_map(48.314775, 25.082925, 2003))
+    print(put_points_on_map(49.8397, 24.0297, 2003))
